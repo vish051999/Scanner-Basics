424ecb675cd6bc476fec116414e15194dbc61853
vish051999 <vishnu.nagar@goguardian.com>
2022-06-22 17:19:40 +0530

    Implemented following two tasks: 1. Implemented a function to spilt pages of pdf into png images and saved the images. 2. Wrote a function to detect QR code in a image and draw a rectangle around the QR code.

---
+++ main.py
@@ -0,0 +1,43 @@
+import fitz
+import cv2
+from pyzbar.pyzbar import decode
+
+pdf_1 = fitz.open("bubble_sheet_292_0.pdf")
+path_1 = "bubble_sheet_292_0_pages"
+
+pdf_2 = fitz.open("bubble_sheet_293_0.pdf")
+path_2 = "bubble_sheet_293_0_pages"
+
+def _pdfToImages(pdf,path):
+    num_pages = pdf.pageCount
+    image_matrix = fitz.Matrix(fitz.Identity)
+    image_matrix.preScale(1, 1)
+    for i in range(num_pages):
+        pix = pdf[1].getPixmap(alpha = False,colorspace=fitz.csGRAY, matrix=image_matrix)
+        pix.writePNG(f"{path}/{i+1}.png")
+
+# _pdfToImages(pdf_1,path_1)
+# _pdfToImages(pdf_2,path_2)
+
+
+img = cv2.imread("bubble_sheet_293_0_pages/1.png") 
+
+def detect_qr(img):
+    qr = decode(img)
+    return qr
+
+qr = detect_qr(img)
+
+
+def draw_rect_qr(qr,img):
+    start = (qr[0][2][0],qr[0][2][1])
+    end = (qr[0][2][0]+qr[0][2][2],qr[0][2][1]+qr[0][2][3])
+    color = (255,0,0)
+    thickness = 5
+
+    qr_img = cv2.rectangle(img,start,end,color,thickness)
+    cv2.imshow("frame",qr_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows("q")
+
+draw_rect_qr(qr,img)
